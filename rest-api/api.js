f8e71278ccf7a7c501522f3fb814ec2428f7bfb1
Raul Ferreira de Lima Ribeiro Novaes <raul.novaes@MJV.net>
2020-08-20 09:57:04 -0300

    Adicionando projeto e base pra desenvolvimento

---
+++ rest-api/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const api = express();
+const porta = 3000;
+const router = express.Router();
+
+api.use(cors());
+
+api.use(bodyparser.urlencoded({extended: true}));
+api.use(bodyparser.json({limit: '20mb', extended: true}));
+
+router.get('/', (req, resp)=> resp.json({
+    mensagem: 'Api Online....'
+}));
+
+api.use('/', router);
+
+api.listen(porta);
+console.log('run api express');
