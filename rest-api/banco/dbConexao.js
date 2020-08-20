5fb077d9ec2dd2a568b94fde6710540d1a5e565f
Raul Ferreira de Lima Ribeiro Novaes <raul.novaes@MJV.net>
2020-08-20 12:11:15 -0300

    arquivos básicos para o crud

---
+++ rest-api/banco/dbConexao.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');
+
+const conexao = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_galeria'
+})
---
+++ rest-api/model/galeria/GaleriaModel.js
@@ -0,0 +1,8 @@
+const db = require('../../banco/dbConexao');
+
+class GaleriaModel {
+
+    static getTodos(callback){
+        return db.query("SELECT * FROM galeria", callback)
+    }
+}
