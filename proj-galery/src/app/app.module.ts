83b4c3ef00fcf9a2fcf117996f4af6ac739a921c
Raul Ferreira de Lima Ribeiro Novaes <raul.novaes@MJV.net>
2020-08-24 10:26:26 -0300

    adicionando componente para galeria e home

--- proj-galery/src/app/app.module.ts
+++ proj-galery/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeCarouselComponent } from './componentes/home/home-carousel/home-carousel.component';
+import { ManterGaleriaComponent } from './componentes/galeria/manter-galeria/manter-galeria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCarouselComponent,
+    ManterGaleriaComponent
   ],
   imports: [
     BrowserModule
